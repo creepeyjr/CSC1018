5ba375f43c9d9dff916776a28feda40db9b7756c
Cian <creepey.jr@gmail.com>
2026-03-01 13:13:49 +0000

    Beginning Lab 4

---
+++ lab4/fibonacci.pl
@@ -0,0 +1,7 @@
+/*
+BRIEF
+-----
+- Write a Prolog relation fib(X,Y) that is true if Y is the Fibonacci Number of X. 
+- The Fibonacci Number of X is the sum of the Fibonacci Numbers of (X-1) and (X-2), for X > 1. 
+- The Fibonacci Number of 1 is 1 and the Fibonacci Number of 0 is 1.
+*/
