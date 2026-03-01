d6a8e21c8c1d4e04c326dce6a51cece3daeb1b42
Cian <creepey.jr@gmail.com>
2026-03-01 13:46:54 +0000

    Completed Fibonacci Recursive question.

--- lab4/fibonacci.pl
+++ lab4/fibonacci.pl
@@ -5,3 +5,16 @@ BRIEF
 - The Fibonacci Number of X is the sum of the Fibonacci Numbers of (X-1) and (X-2), for X > 1. 
 - The Fibonacci Number of 1 is 1 and the Fibonacci Number of 0 is 1.
 */
+
+/* Base Cases */
+fib(0, 1).
+fib(1, 1).
+
+/* Recursive Case */
+fib(X, Y) :-
+    X > 1,  % Base case
+    X1 is X - 1,
+    X2 is X - 2,
+    fib(X1, F1),
+    fib(X2, F2),
+    Y is F1 + F2.
