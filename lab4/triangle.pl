a917449e85437d9847d6148b526f325561a1f98f
Cian <creepey.jr@gmail.com>
2026-03-03 15:35:20 +0000

    Completed lab 4

--- lab4/triangle.pl
+++ lab4/triangle.pl
@@ -11,7 +11,41 @@ For example, the clause 6/4 tA X is true if the value X is 12.
 PLAN
 ----
 - Figure out and define an infix binary operator.
+    - Preceence 200 (relatively low priority)
+    - xfx (non-associative, no chaining)
+    - Operator name tA
 - So with the example 6/4, if 6 (base) and 4 (perpendicular) have an area = to X (12)
 - Then it is true.
+
+EXPLANATION : xfx
+- Cannot chain expressions. E.g. a tA b tA c, 
+    - x means the argument must have lower precedence than the operator
+    - y means the argument can have equal or lower precedence
+    - f is the position of the operator itself
+
 */
 
+/* Operator Facts */
+:- op(200, xfx, tA).
+
+/* 
+Calculation
+-----------
+A : Base
+B : Height
+Output of calcuation : Area
+*/
+
+A/B tA Area :-
+    Area is (A * B) / 2.
+
+/* 
+Test Prompts
+------------
+?- 6/4 tA X.
+X = 12.
+
+?- 99/7 tA X.
+X = 346.5.
+
+*/
