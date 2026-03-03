6f89e60ecabea9374b1ff619da6a8742417a4f7a
Cian <creepey.jr@gmail.com>
2026-03-03 15:41:13 +0000

    Creation of lab 5

---
+++ lab5/numbers.pl
@@ -0,0 +1,8 @@
+/*
+BRIEF
+-----
+1. Classifying Numbers
+Write a Prolog relation class(X,Y) that classifies a number X into a category Y,
+where Y is either negative, zero or positive. Use cuts to make it more efficient. 
+*/
+
